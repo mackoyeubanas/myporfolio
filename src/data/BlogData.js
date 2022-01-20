deda3fbae2193d65efb176a4f7e677a54f3ffe99
Mackoy Eubanas <meubanas@gmail.com>
2022-01-20 15:56:19 +0800

    Porfolio

---
+++ src/data/BlogData.js
@@ -0,0 +1,74 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
+        tags:["react","gsap","styled-components"],
+        date:"10 Jan, 2020",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"https://www.youtube.com/watch?v=s-_8wRbuPSM"
+    },
+    {
+        id:2,
+        name:"How to create React Hamburger Menu using Styled-Components",
+        tags:["react","styled-components"],
+        date:"7 August, 2020",
+        imgSrc:"https://i.ytimg.com/vi/GGkBwpxV7AI/maxresdefault.jpg",
+        link:"https://www.youtube.com/watch?v=GGkBwpxV7AI"
+        
+    },
+    {
+        id:3,
+        name:"React Loading Screen: Try these 3 cool loading screens for your app",
+        tags:["react","react-lottie","styled-components"],
+        date:"16 May, 2021",
+        imgSrc:"https://i.imgur.com/epW4vOb.jpeg",
+        link:"https://www.youtube.com/watch?v=PNQGbczXkMI"
+        
+    },
+    {
+        id:4,
+        name:"How to build a Fabulous Todo App using React, Redux and Framer-Motion",
+        tags:["react","redux","framer-motion"],
+        date:"26 May, 2021",
+        imgSrc:"https://i.ytimg.com/vi/0c0v_40MPq8/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLAwfUfKJv-XsgRNDENCYKMmqklTGg",
+        link:"https://www.youtube.com/watch?v=xwkbWUOGfws"
+        
+    },
+    {
+        id:5,
+        name:"What is Higher Order Component (HOC) in React JS?",
+        tags:["react","reactJS","web-development"],
+        date:"28 March, 2021",
+        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        link:"https://www.youtube.com/watch?v=B6aNv8nkUSw"
+    },
+    {
+        id:6,
+        name:"How to implement Pagination Component in React from scratch",
+        tags:["react","css"],
+        date:"14 Oct, 2021",
+        imgSrc:"https://res.cloudinary.com/academind-gmbh/image/upload/f_auto,q_auto:eco/dpr_2.0,w_400,c_fit,g_center/v1/academind.com/content/tutorials/reactjs-pagination/react-pagination-thumbnail",
+        link:"https://www.youtube.com/watch?v=kMuRr53RjcE"
+
+    },
+   
+    {
+        id:7,
+        name:"What is Redux ? Simply Explained!",
+        tags:["react","redux","javascript"],
+        date:"16 Nov, 2021",
+        imgSrc:"https://miro.medium.com/max/2000/1*l9GqESHUyb9CHMfBHnnh8g.png",
+        link:"https://www.youtube.com/watch?v=D2MqT4OEgoE"
+    },
+    {
+        id:8,
+        name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+        tags:["react","framer-motion","styled-components"],
+        date:"7 Dec, 2021",
+        imgSrc:"https://techyblinder.com/wp-content/uploads/2021/05/Navigation-Menu-3.gif",
+        link:"https://www.youtube.com/watch?v=5I_uGo8-jPs"
+    },
+  
+] 
